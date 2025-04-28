bf3f6ce78c8377485474ee410c14f989a017469f
joel-CM <joelchavezm19@gmail.com>
2025-04-28 20:29:29 -0300

    python: clase 3

---
+++ programacion-I/python/clase3/ejercicio_1.py
@@ -0,0 +1,12 @@
+""" 
+Califica tu día 
+
+¿Cómo estuvo tu día (1 al 10)?
+
+Mi día estuvo de: 10
+
+Hacer el código"""
+
+print("Califica tu dia")
+dia = int(input("¿Como estuvo tu dia (1 al 10): "))
+print(f"Mi dia estuvo de: {dia}")  
