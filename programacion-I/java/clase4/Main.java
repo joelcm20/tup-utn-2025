14e6f907e964c9000a59de9e7020f5e01dbb6fcc
joel-CM <joelchavezm19@gmail.com>
2025-05-01 16:27:58 -0300

    java: clase 4, datos primitivos, enteros y flotantes

---
+++ programacion-I/java/clase4/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipos de datos primitivos enteros
+
+        // BYTE
+        Byte numeroByte = 127;
+        System.out.println("Ejemplo de byte: " + numeroByte);
+        System.out.println("Minimo de byte: " + Byte.MIN_VALUE);
+        System.out.println("Maximo de byte: " + Byte.MAX_VALUE);
+
+        // SHORT
+        Short numeroShort = 100;
+        System.out.println("\nEjemplo de short: " + numeroShort);
+        System.out.println("Minimo de short: " + Short.MIN_VALUE);
+        System.out.println("Maximo de short: " + Short.MAX_VALUE);// SHORT
+
+        int numeroInt = 4567;
+        System.out.println("\nEjemplo de int: " + numeroInt);
+        System.out.println("Minimo de int: " + Integer.MIN_VALUE);
+        System.out.println("Maximo de int: " + Integer.MAX_VALUE);
+
+        long numeroLong = 4567;
+        System.out.println("\nEjemplo de long: " + numeroLong);
+        System.out.println("Minimo de long: " + Long.MIN_VALUE);
+        System.out.println("Maximo de long: " + Long.MAX_VALUE);
+
+        // Tipos de datos primitivos flotantes
+
+        // float
+        float numeroFloat = 34.456F;
+        System.out.println("\nEjemplo de float: " + numeroFloat);
+        System.out.println("Minimo de float: " + Float.MIN_VALUE);
+        System.out.println("Maximo de float: " + Float.MAX_VALUE);
+
+        // double
+        double numeroDouble = 347.53745;
+        System.out.println("\nEjemplo de double: " + numeroDouble);
+        System.out.println("Minimo de double: " + Double.MIN_VALUE);
+        System.out.println("Maximo de double: " + Double.MAX_VALUE);
+    }
+}
