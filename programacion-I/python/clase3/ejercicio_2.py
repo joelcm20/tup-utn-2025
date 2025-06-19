38d2eb75fb6427137d215ba0b24d94b021830f57
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2025-06-18 23:30:37 -0300

    python: clase 3

---
+++ programacion-I/python/clase3/ejercicio_2.py
@@ -0,0 +1,23 @@
+""" 
+Se solicita incluir la siguiente información acerca de un libro:
+
+título
+
+autor
+
+Debes imprimir la información en el siguiente formato:
+
+Proporciona el título:
+
+Proporciona el autor:
+
+<título> fue escrito por <autor>
+
+Hacer el código"""
+
+print("Proporciona el titulo:")
+titulo = input()
+print("Proporciona el autor:")
+autor = input()
+print(f"{titulo} fue escrito por {autor}")
+
