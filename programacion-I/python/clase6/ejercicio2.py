fddb1e900edcc9bf73f801411b40f430be76e6b6
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2025-06-18 23:32:59 -0300

    (refactor)python: clase 6, ejercicio 2

--- programacion-I/python/clase6/ejercicio2.py
+++ programacion-I/python/clase6/ejercicio2.py
@@ -4,7 +4,8 @@ Determinar la solución lógica de la siguiente operación.
 
 ((3 + 5 x 8 ) < 3 and ((- 6/3 x 4 ) + 2 < 2)) or ( a > b)
 """
-
-resultado = ((3 + 5 * 8) < 3 and ((-6/3 * 4) + 2 < 2)) or (3 > 4)
+a = 4
+b = 3
+resultado = ((3 + 5 * 8) < 3 and ((-6/3 * 4) + 2 < 2)) or (a > b)
 
 print(f"El resultado es: {resultado}")
